36944892e9635af00717d15265548eb8891bf99d
Anissa Crawford <anissacrawford@gmail.com>
2022-02-03 15:43:48 -0600

    finishing question 9

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -50,7 +50,7 @@ for (let i=0; i<6; i++){
     console.log('Removed ' + supplyChanges[i] + ' parts');
   }
 }
-// console.log(supplyChanges[i]); Would show numbers alongside sentences 
+// console.log(supplyChanges[i]); Would show numbers alongside sentences
 
 
 // STRETCH GOALS
@@ -59,20 +59,28 @@ console.log('---  Stretch Goals  ---');
 console.log('7. Showing supplyChanges with "for of" loop');
 
 for (number of supplyChanges){
-  console.log(number);
+  if (number > 0 ) {
+  console.log(`Added ${number} parts`);
+} else if (number === 0) {
+  console.log('No changes');
+} else if (number < 0) {
+  console.log(`Removed ${number} parts`);
+} else {
+  console.log('Undefined');
+}
 }
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
 
+let sum = 0;
 
+for (let i=0; i < supplyChanges.length; i++) {
+  sum += supplyChanges[i];
+}
 
-//let sum = 0
-//for (let i=0; i < supplyChanges.length; i++) {
-//  console.log(sum += supplyChanges[i]);
-//}
-
+console.log(sum);
 
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
@@ -81,3 +89,13 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let i = 572;
+let max = 0;
+let boxes = 7;
+
+while (i > max ) {
+  i += boxes;
+  console.log(boxes);
+  i--;
+}
